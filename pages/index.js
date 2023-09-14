2690028b70a582fc71e774bd4ab91424614e3562
Matheus Lorenzeto <matheus.mlorenzeto@gmail.com>
2023-09-14 01:34:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Por que o zagueiro, quando se aposenta, é o jogador que deixa mais saudades?</h1>
+  return (
+    <h1>
+      Por que o zagueiro, quando se aposenta, é o jogador que deixa mais
+      saudades?
+    </h1>
+  );
 }
 
 export default Home;
