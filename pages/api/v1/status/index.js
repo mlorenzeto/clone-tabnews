17dd1488c35b58a899a98c2a2372a52e4e97a230
mlorenzeto <matheus.mlorenzeto@gmail.com>
2024-07-06 14:02:42 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const { query } = database;
+  // Recupera a versão do Postgres
+  const databaseVersionResult = await query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  // Recupera o total de conexões disponíveis
+  const databaseMaxConnectionsResult = await query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  // Recupera as conexões abertas
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await query({
+    text: "SELECT count(*)::int total_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].total_connections;
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
