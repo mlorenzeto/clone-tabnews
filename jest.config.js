9080b67212b8d2a7cfdce7841e6cadb9b3ed2de7
Matheus Lorenzeto <matheus.mlorenzeto@gmail.com>
2024-08-06 00:00:14 +0000

    adds `/api/v1/migrations` endpoint

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-/** @returns {Promise<import('jest').Config>} */
-module.exports = async () => {
-  return {
-    modulePathIgnorePatterns: [".next", "node_modules"],
-  };
-};
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
