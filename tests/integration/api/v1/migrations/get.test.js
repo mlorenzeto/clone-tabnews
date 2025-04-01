d1194f870b3f8bc91e52bee9bdf9a0c2b6695cdb
Matheus Lorenzeto <matheus.mlorenzeto@gmail.com>
2025-03-31 23:39:30 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -5,14 +5,17 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  expect(response.status).toBe(200);
+      const responseBody = await response.json();
 
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,27 +3,32 @@ import orchestrator from "tests/orchestrator";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  expect(response.status).toBe(200);
+      const responseBody = await response.json();
 
-  const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(typeof responseBody.dependencies.database.max_connections).toBe(
+        "number",
+      );
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
-  expect(typeof responseBody.dependencies.database.max_connections).toBe(
-    "number",
-  );
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-
-  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
-    "number",
-  );
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeDefined();
+      expect(typeof responseBody.dependencies.database.opened_connections).toBe(
+        "number",
+      );
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
